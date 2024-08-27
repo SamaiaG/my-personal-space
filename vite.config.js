cc15fc4fbc38971df9450cc7be1be36135c50239
Samaia Gahramanov <samaiag96@gmail.com>
2024-08-27 16:11:28 +0200

    one more try

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
-// vite.config.js
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  // Define base path based on the environment
-  const isProduction = command === 'build';
-  const base = isProduction ? '/my-personal-space/' : '/';
-
-  return {
-    plugins: [vue()],
-    base: base, // Set base path based on environment
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  base: '/my-personal-space/' || '/',
+   resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  };
-});
+  } 
+})
