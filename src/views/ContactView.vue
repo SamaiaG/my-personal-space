9de2b376a47a077552aae79c4ce54f481e7fab9f
Samaia Gahramanov <samaiag96@gmail.com>
2024-08-27 15:37:19 +0200

    first commit here

---
+++ src/views/ContactView.vue
@@ -0,0 +1,124 @@
+<template>
+  <BaseSection class="contact-section">
+    <div class="contactContainer">
+      <h2 class="contactTitle">Let's get in touch</h2>
+      <p class="contactMessage">
+        I am available for work. <br />
+        You can write me a message,
+        <a class="gmail" href="mailto:samaiagahramanov@gmail.com?subject=Hello from your website">
+          email me directly</a
+        >
+        or find me on
+        <a href="https://www.linkedin.com/in/samaia-gahramanov-569343232/"> Linkedin</a>.
+      </p>
+      <form class="contactForm">
+        <div class="form-fields">
+          <div class="name-and-email">
+            <div class="name">
+              <div class="form-floating mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="firstname"
+                  required
+                />
+                <label for="floatingInput">First Name</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="lastname"
+                  required
+                />
+                <label for="floatingInput">Last Name</label>
+              </div>
+            </div>
+            <div class="email form-floating mb-3">
+              <input
+                type="email"
+                class="form-control"
+                id="floatingInput"
+                placeholder="email"
+                required
+              />
+              <label for="floatingInput">Email</label>
+            </div>
+          </div>
+          <div class="message form-group mb-3">
+            <textarea
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              placeholder="Enter your message"
+              required
+            ></textarea>
+          </div>
+        </div>
+        <BaseButton type="submit" class="submit-btn">Send</BaseButton>
+      </form>
+    </div>
+  </BaseSection>
+</template>
+
+<script setup>
+import BaseSection from '@/components/BaseSection.vue'
+import BaseButton from '@/components/BaseButton.vue'
+</script>
+<style scoped>
+.contactContainer {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  color: #393737;
+  gap: 3vmin;
+}
+.contactTitle {
+  font-size: 5vmin;
+  font-weight: 600;
+  font-family: 'Raleway', sans-serif;
+  text-align: center;
+}
+.contactMessage {
+  font-size: 2vmin;
+  font-weight: 400;
+  font-family: 'Raleway', sans-serif;
+  line-height: 120%;
+  letter-spacing: 0.1vmin;
+  text-align: center;
+}
+.contactForm {
+  display: block;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 8vmin;
+}
+
+.name {
+  display: flex;
+  justify-content: space-between;
+  gap: 5vmin;
+}
+.name-and-email {
+  display: flex;
+  flex-direction: column;
+}
+.form-fields {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 5vmin;
+  margin: 5vmin 0;
+}
+.form-control {
+  width: 100%;
+  font-size: 2vmin;
+  font-family: 'Raleway', sans-serif;
+  padding: 2vmin 4vmin;
+}
+.form-floating {
+  width: 100%;
+}
+</style>
---
+++ src/views/ListedPortfolioView.vue
@@ -0,0 +1,177 @@
+<template>
+  <BaseSection class="projects">
+    <h1>Some projects</h1>
+    <div class="bd-example">
+      <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img
+              class="d-block w-100"
+              alt="First slide [800x400]"
+              src="@/assets/images/projects_covers/cover1.png"
+              data-holder-rendered="true"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              class="d-block w-100"
+              alt="Second slide [800x400]"
+              src="@/assets/images/projects_covers/cover2.png"
+              data-holder-rendered="true"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              class="d-block w-100"
+              alt="Third slide [800x400]"
+              src="@/assets/images/projects_covers/cover3.png"
+              data-holder-rendered="true"
+            />
+          </div>
+        </div>
+        <button class="carousel-control-prev" href="#" role="button">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button class="carousel-control-next" href="#" role="button">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
+      </div>
+    </div>
+    <RouterLink to="/projects" class="h-link">see more projects...</RouterLink>
+  </BaseSection>
+  <BaseSection class="css-projects">
+    <h1>Some CSS Fun</h1>
+    <div class="bd-example">
+      <div id="carouselExampleIndicatorsCSS" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <iframe
+              class="d-block w-100"
+              alt="First slide [800x400]"
+              src="/100 days of CSS/girl/girl.html"
+              title="girl"
+              data-holder-rendered="true"
+            ></iframe>
+          </div>
+          <div class="carousel-item">
+            <iframe
+              class="d-block w-100"
+              alt="Second slide [800x400]"
+              src="/100 days of CSS/flower pot/index.html"
+              title="girl"
+              data-holder-rendered="true"
+            ></iframe>
+          </div>
+          <div class="carousel-item">
+            <iframe
+              class="d-block w-100"
+              alt="Third slide [800x400]"
+              src="/100 days of CSS/anxiety/index.html"
+              title="girl"
+              data-holder-rendered="true"
+            ></iframe>
+          </div>
+        </div>
+        <button class="carousel-control-prev" href="#" role="button">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button class="carousel-control-next" href="#" role="button">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
+      </div>
+    </div>
+    <RouterLink to="/cssfun" class="h-link">see more css fun tries...</RouterLink>
+  </BaseSection>
+</template>
+
+<script setup>
+import BaseSection from '@/components/BaseSection.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const carousel = document.getElementById('carouselExampleIndicators')
+  const carouselCSS = document.getElementById('carouselExampleIndicatorsCSS')
+
+  const items = carousel.querySelectorAll('.carousel-item')
+  const itemsCSS = carouselCSS.querySelectorAll('.carousel-item')
+
+  let currentIndex = 0
+  let currentIndexCSS = 0
+
+  function showSlide(index) {
+    items.forEach((item, i) => {
+      item.classList.toggle('active', i === index)
+    })
+  }
+  function showSlideCSS(index) {
+    itemsCSS.forEach((item, i) => {
+      item.classList.toggle('active', i === index)
+    })
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % items.length
+    showSlide(currentIndex)
+  }
+  function nextSlideCSS() {
+    currentIndexCSS = (currentIndexCSS + 1) % itemsCSS.length
+    showSlideCSS(currentIndexCSS)
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + items.length) % items.length
+    showSlide(currentIndex)
+  }
+  function prevSlideCSS() {
+    currentIndexCSS = (currentIndexCSS - 1 + itemsCSS.length) % itemsCSS.length
+    showSlideCSS(currentIndexCSS)
+  }
+
+  carousel.querySelector('.carousel-control-next').addEventListener('click', nextSlide)
+  carousel.querySelector('.carousel-control-prev').addEventListener('click', prevSlide)
+
+  carouselCSS.querySelector('.carousel-control-next').addEventListener('click', nextSlideCSS)
+  carouselCSS.querySelector('.carousel-control-prev').addEventListener('click', prevSlideCSS)
+
+  // Initialize the first slide as active
+  showSlide(0)
+  showSlideCSS(0)
+})
+</script>
+
+<style scoped>
+.carousel-item {
+  display: none;
+}
+.carousel-inner {
+  width: 100vmin;
+}
+
+.carousel-item.active {
+  display: block;
+}
+.d-block {
+  height: 400px;
+  width: 800px;
+}
+.carousel-inner {
+  padding: 0 10vmin;
+}
+.carousel-control-prev-icon {
+  background-image: url('@/assets/images/chevron-left.svg');
+}
+.carousel-control-next-icon {
+  background-image: url('@/assets/images/chevron-right.svg');
+}
+.h-link {
+  color: #f4a460;
+  font-family: 'Raleway', sans-serif;
+}
+.h-link:hover {
+  color: #f16529;
+}
+h1 {
+  font-family: 'Raleway', sans-serif;
+  letter-spacing: 0.1vmin;
+}
+</style>
