8297e98cbb72c12eab7087997dc5d40d8168f009
Samaia Gahramanov <samaiag96@gmail.com>
2024-10-01 15:55:52 +0200

    deploy try

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/',
+  base: '/my-personal-space/',
    resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
